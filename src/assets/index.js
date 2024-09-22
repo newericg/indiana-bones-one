e980b2cd7f020e4d8cfc1c68bc2706e07fbf70c2
newericg1~ <eric.gomes0105@gmail.com>
2024-09-22 00:33:55 -0300

    fix: hero video to img and text fixes

--- src/assets/index.js
+++ src/assets/index.js
@@ -113,10 +113,10 @@ import dogHikingPageImg from './new-pictures/dog-1.png'
 import dogHikingPageImg2 from './new-pictures/dog-2.png'
 import dogHikingPageImgHead from './new-pictures/dog-hiking-section.jpg'
 
-import videoHero1 from './videos/hero-background-one.MP4'
+// import videoHero1 from './videos/hero-background-one.MP4'
 
-import heroVertical from './videos/hero-oficial-vertical.MP4'
-import heroHorizontal from './videos/hero-oficial-horizontal.MP4'
+// import heroVertical from './videos/hero-oficial-vertical.MP4'
+// import heroHorizontal from './videos/hero-oficial-horizontal.MP4'
 
 
 
