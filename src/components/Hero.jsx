f463d5e497322d70335deab58d5e15858cb40baa
newericg1~ <eric.gomes0105@gmail.com>
2024-09-23 20:03:40 -0300

    new video build test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import CompanyLogos from "./CompanyLogos";
 import Section from "./Section";
 import { BackgroundCircles, BottomLine } from "./design/Hero";
 import Reveal from "../libs/Reveal";
-// import  {videoHero1, heroHorizontal, heroVertical}  from "../assets";
+import  {heroHorizontal, heroVertical}  from "../assets";
 import AutoPlaySilentVideo from "./VideoComponent";
 
 const Hero = () => {
@@ -19,7 +19,7 @@ const Hero = () => {
       customPaddings
       verticalLines
     >
-      <div className="my-15 container relative" ref={parallaxRef}>
+      {/* <div className="my-15 container relative" ref={parallaxRef}>
         <div>
           <Reveal>
             <div className="relative z-10 max-w-[62rem] mx-auto text-center mb-[3.875rem] lg:mb-[6.25rem]">
@@ -64,25 +64,25 @@ const Hero = () => {
             <BackgroundCircles />
           </div>
         </div>
-      </div>
-      {/* <div className="h-screen w-screen overflow-hidden"> */}
-        {/* <AutoPlaySilentVideo
-          video="https://ijwrynw7lgxgxcmm.public.blob.vercel-storage.com/hero-oficial-horizontal-muted-gi8OyCaHmkfRlncqZQio0RfU0el1IV.mp4"
+      </div> */}
+      <div className="h-screen w-screen overflow-hidden">
+        <AutoPlaySilentVideo
+          video={heroVertical}
           className="hidden sm:block h-[85vh] w-screen m-0 p-0 object-cover"
-        ></AutoPlaySilentVideo> */}
+        ></AutoPlaySilentVideo>
         {/* <video playsInline autoPlay muted loop id="myVideo" className="hidden sm:block h-[85vh] w-screen m-0 p-0 object-cover">
            <source src="https://ijwrynw7lgxgxcmm.public.blob.vercel-storage.com/hero-oficial-horizontal-ipsYG48QCVTPeJ1HWlnMO96xrf2p9f.MP4" type="video/mp4" />
          </video> */}
 
-        {/* <AutoPlaySilentVideo
-          video="https://ijwrynw7lgxgxcmm.public.blob.vercel-storage.com/hero-oficial-vertical-muted-1ndCqsuIUbcQLk4KnGymWX7IELy2cL.mp4"
+        <AutoPlaySilentVideo
+          video={heroHorizontal}
           className="sm:hidden h-[90vh] w-screen m-0 p-0 object-cover"
-        ></AutoPlaySilentVideo> */}
+        ></AutoPlaySilentVideo>
 
         {/* <video playsInline autoPlay muted loop id="myVideo" className="sm:hidden h-[90vh] w-screen m-0 p-0 object-cover">
           <source playsInline src="https://ijwrynw7lgxgxcmm.public.blob.vercel-storage.com/hero-oficial-vertical-YuJVa5BFSkhwAUAAJtLLNHm7ntzeXy.mp4" type="video/mp4" />
         </video> */}
-      {/* </div> */}
+      </div>
       <Reveal>
         <CompanyLogos className="relative sm:mb-20 sm:mt-32 company-logos w-screen flex flex-col items-center" />
       </Reveal>
